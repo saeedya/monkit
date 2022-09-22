6fd87aa1ba87c238e5d960b8d84e41eab5bc0e79
saeed <saeed@local>
2022-09-23 00:41:28 +0330

    main.py updated

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from urllib.request import urlopen
 from fastapi import FastAPI
 from fastapi_health import health
 from now import now
-import psutil, os, socket
+import psutil
+import os
+import subprocess
 
 
 app = FastAPI()
@@ -15,11 +17,20 @@ def root():
 
 def status():
 
+    def get_ip_address():
+
+        proc = subprocess.Popen(
+            "hostname -I | awk '{print $1}'", stdout=subprocess.PIPE, shell=True)
+        p = proc.communicate()[0]
+        ip_address = p.strip().decode("utf-8")
+        return ip_address
+
     process = psutil.Process(os.getpid())
     active_connections = len(process.connections())
-    ip_address = socket.gethostbyname(socket.gethostname())
-    memory = process.memory_percent()
-    cpu = process.cpu_percent()
+    memory_available = ((psutil.virtual_memory()[4] / 1024) / 1024) / 1024
+    load1 = psutil.getloadavg()[0]
+    cpu_usage = (load1/os.cpu_count()) * 100
+    cpu_available = 100 - cpu_usage
     current_datetime = now()
 
     code = urlopen("http://localhost:8000").getcode()
@@ -28,9 +39,9 @@ def status():
         return {
             "status": "up",
             "active_connections": active_connections,
-            "ip_address": ip_address,
-            "memory_total": memory,
-            "cpu_total": cpu,
+            "ip_address": get_ip_address(),
+            "memory_total": memory_available,
+            "cpu_total": cpu_available,
             "current_datetime": current_datetime
         }
 
