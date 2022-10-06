91c5e4ec8792f09796943bbeac176420ab48182b
saeed <saeed@local>
2022-10-06 18:35:13 +0330

    main.py updated

--- monkit/main.py
+++ monkit/main.py
@@ -6,7 +6,6 @@ import psutil
 import os
 import subprocess
 
-
 app = FastAPI()
 
 
@@ -16,9 +15,7 @@ def root():
 
 
 def status():
-
     def get_ip_address():
-
         proc = subprocess.Popen(
             "hostname -I | awk '{print $1}'", stdout=subprocess.PIPE, shell=True)
         p = proc.communicate()[0]
@@ -27,10 +24,8 @@ def status():
 
     process = psutil.Process(os.getpid())
     active_connections = len(process.connections())
-    memory_available = ((psutil.virtual_memory()[4] / 1024) / 1024) / 1024
-    load1 = psutil.getloadavg()[0]
-    cpu_usage = (load1/os.cpu_count()) * 100
-    cpu_available = 100 - cpu_usage
+    memory_available = psutil.virtual_memory()[2]
+    cpu_available = 100 - psutil.cpu_percent()
     current_datetime = now()
 
     code = urlopen("http://localhost:8000").getcode()
@@ -40,8 +35,8 @@ def status():
             "status": "up",
             "active_connections": active_connections,
             "ip_address": get_ip_address(),
-            "memory_total": memory_available,
-            "cpu_total": cpu_available,
+            "memory_total": str(memory_available) + "%",
+            "cpu_total": str(cpu_available) + "%",
             "current_datetime": current_datetime
         }
 
