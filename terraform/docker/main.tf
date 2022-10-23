ae37df33093cb02959f33734bc825d77188a143a
saeed <saeed@local>
2022-10-23 11:53:55 +0330

    Some terraform codes changed

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -20,9 +20,26 @@ resource "docker_image" "monkit" {
   name = "monkit"
   build {
     path = "../.."
-    tag  = ["localhost:5000/monkit"]
+    tag  = ["127.0.0.1:5000/monkit"]
     label = {
       author : "saeedya"
     }
+  }
+}
+
+resource "docker_image" "monkit_image" {
+  name = "127.0.0.1:5000/monkit:latest"
+}
+
+# Create monkit container and published on port 8000
+resource "docker_container" "monkit_container" {
+  provider     = docker
+  image        = docker_image.monkit_image.name
+  name         = "monkit"
+  attach       = false
+  network_mode = "host"
+  ports {
+    internal = 8000
+    external = 8000
   }
 }
